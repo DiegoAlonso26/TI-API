776d83ff41d93d4962137e85d304ecaa038be2b3
juanj <juanjosejey2003@gmail.com>
2024-06-24 13:13:20 -0500

    arregle la wuebada de diego

--- src/main/java/upeu/tiapi/Controlador/ProductoControlador.java
+++ src/main/java/upeu/tiapi/Controlador/ProductoControlador.java
@@ -2,6 +2,7 @@ package upeu.tiapi.Controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import upeu.tiapi.Entity.Producto;
